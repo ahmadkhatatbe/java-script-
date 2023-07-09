202d43ff8a31399d3db7ee7bfd922cc36b3090d3
ahmadkhatatbe <ahmedkatatbeh123@gmail.comgit config --global user.email ahmedkatatbeh123@gmail.com>
2023-07-09 22:33:49 +0300

    Co-authored-by: ahmadkhatatbe <ahmedkatatbeh123@gmail.comgit config --global user.email ahmedkatatbeh123@gmail.com>

--- 7-9-2023/javascript.js
+++ 7-9-2023/javascript.js
@@ -107,5 +107,5 @@ Sample array : var arr1 = [ -3, 8, 7, 6, 5, -4, 3, 2, 1 ];
 Sample Output : -4,-3,1,2,3,5,6,7,8
 */
 var arr1 = [ -3, 8, 7, 6, 5, -4, 3, 2, 1 ];
-arr1.sort;
+arr1.sort(function(a, b){return a - b});
 console.log(arr1);
